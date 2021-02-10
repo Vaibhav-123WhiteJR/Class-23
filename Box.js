d61dce47de51793bec193b3c1b94703382b1d314
Vaibhav-123WhiteJR <77727779+Vaibhav-123WhiteJR@users.noreply.github.com>
2021-02-10 17:00:09 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box
+{
+    constructor(x,y,width,height)
+    {
+        var box_options ={
+            restitution: 0.8
+        }
+    
+        this.body = Bodies.rectangle(x,y,width,height, box_options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+
+    display()
+    {   var angle =this.body.angle;
+        var pos =this.body.position;
+        angleMode(RADIANS);
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0, this.width,this.height);
+        pop();
+    }
+}
